ee38888de1677cbd74fad222c8595036532a85d6
Alena Katkova <alena.katkova1996@gmail.com>
2023-07-06 21:53:57 +0300

    Set up gitignore

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 20_000;
+        int miles = 20;
+        int bonus = amount / miles;
+
+        System.out.println("За "  + amount + " рублей начислено " + bonus + " миль. ");
+    }
+}
